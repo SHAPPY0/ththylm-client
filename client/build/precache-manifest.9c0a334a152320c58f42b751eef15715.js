0d6b2bd9bbf6edf854ddb3892c09dd0ae1f37c3d
saurabh <sharmasaurabh450@gmail.com>
2022-12-19 11:20:30 +0530

    init

---
+++ client/build/precache-manifest.9c0a334a152320c58f42b751eef15715.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b720898144cd15fe56ffb3380179954",
+    "url": "/index.html"
+  },
+  {
+    "revision": "598b783b1866bbf34b80",
+    "url": "/static/css/main.c5875d82.chunk.css"
+  },
+  {
+    "revision": "3f94d1a61e34dcd447f4",
+    "url": "/static/js/2.0247849b.chunk.js"
+  },
+  {
+    "revision": "de46a224716c10699c6f389ec89ca794",
+    "url": "/static/js/2.0247849b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "598b783b1866bbf34b80",
+    "url": "/static/js/main.c71a1adc.chunk.js"
+  },
+  {
+    "revision": "561824c4461e7a62da3a",
+    "url": "/static/js/runtime-main.5433dd00.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ index.js
@@ -0,0 +1,24 @@
+
+const express = require('express');
+const path = require('path');
+const cors			    = require('cors');
+const app = express();
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5001;
+app.listen(port);
+
+console.log('App is listening on port ' + port);
